9a3006e03254b59bdda54d67a3d45e96404089a8
Hemesh Patel <hemesh.v.patel@gmail.com>
2019-05-14 22:53:13 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -28,7 +28,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(1);
+  return array.slice(1);
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
